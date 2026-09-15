a915658e0b342b8ac5f121979baf0c4faacaef33
smondoe <smondoe@outlook.jp>
2026-09-15 20:43:42 +0900

    eslint error

--- react-router/app/routes/map.tsx
+++ react-router/app/routes/map.tsx
@@ -48,7 +48,6 @@ export default function MapPage() {
 	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 	const [query, setQuery] = useState("");
 	const [tracks, setTracks] = useState<PlayableTrack[]>([]);
-	const [selectedTrack, setSelectedTrack] = useState<PlayableTrack>();
 	const [area, setArea] = useState<LatLngTuple[]>([]);
 	const [location, setLocation] = useState<Location>();
 	const [isInside, setIsInside] = useState(false);
